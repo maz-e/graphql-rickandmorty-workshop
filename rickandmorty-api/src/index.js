767dd6d6de6ebc72264762225767db7dd7396011
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-08-04 21:27:27 +0200

    feat: replace the mock data using the api rest and add character/s fetch functions

--- rickandmorty-api/src/index.js
+++ rickandmorty-api/src/index.js
@@ -1,30 +1,10 @@
 const fetch = require("node-fetch")
 const { ApolloServer, gql } = require("apollo-server")
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling"
-  },
-  {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
-]
-
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
 
   enum CharacterStatus {
     Alive
@@ -36,13 +16,14 @@ const typeDefs = gql`
     id: ID
     name: String
     status: CharacterStatus
-    episodes: [String]
+    image: String
+    episode: [String]
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    character(id: ID!): Character
     characters: [Character]
   }
 `
@@ -51,10 +32,24 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
-    characters: () => characters
+    characters: () => fetchCharacters(),
+    character: (parent, args) => {
+      const { id } = args
+      return fetchCharacterById(id)
+    }
   }
 }
+
+const fetchCharacters = () => {
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then(res => res.json())
+    .then(json => json.results)
+}
+
+const fetchCharacterById = (id) => {
+  return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(res => res.json())
+} 
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
@@ -66,77 +61,3 @@ const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
-
-// mock data for characters
-const characters = [
-  {
-    name: "Rick Sanchez",
-    id: 1,
-    status: "Alive",
-    episodes: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2"
-    ]
-  },
-  {
-    name: "Morty Smith",
-    id: 2,
-    status: "Alive",
-    episodes: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/3"
-    ]
-  }
-]
-
-// mock data for episodes
-const episodes = [
-  {
-    name: "Pilot",
-    id: 1
-  },
-  {
-    name: "Lawnmower Dog",
-    id: 2
-  }
-]
-
-function fetchEpisodes() {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch("https://rickandmortyapi.com/api/episode/")
-    .then(res => res.json())
-    .then(json => json.results)
-}
-
-function fetchEpisodeById(id) {
-  return fetch("https://rickandmortyapi.com/api/episode/" + id)
-    .then(res => res.json())
-    .then(json => json)
-}
-
-function fetchEpisodeByUrl(url) {
-  return fetch(url)
-    .then(res => res.json())
-    .then(json => json)
-}
-
-function fetchCharacters() {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch("https://rickandmortyapi.com/api/character/")
-    .then(res => res.json())
-    .then(json => json.results)
-}
-
-function fetchCharacterById(id) {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch("https://rickandmortyapi.com/api/character/" + id)
-    .then(res => res.json())
-    .then(json => json)
-}
-
-function fetchCharacterByUrl(url) {
-  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
-  return fetch(url)
-    .then(res => res.json())
-    .then(json => json) 
-}
